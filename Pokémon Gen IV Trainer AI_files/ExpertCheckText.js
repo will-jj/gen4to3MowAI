f37621614c21f2fecde69af4ab1a0a7f66ee92e9
William Jackson <will-jj@users.noreply.github.com>
2025-07-04 09:18:15 +0100

    SolarBeam: No 0x

--- Pokémon Gen IV Trainer AI_files/ExpertCheckText.js
+++ Pokémon Gen IV Trainer AI_files/ExpertCheckText.js
@@ -923,7 +923,7 @@ Otherwise:
 
 
 ["Solarbeam"]:
-`If the effectiveness of the move is 1/2x, 1/4x, or 0x:
+`If the effectiveness of the move is 1/2x, or 1/4x:
 	Score -2 and terminate
 
 If the target knows the move Protect or Detect:
