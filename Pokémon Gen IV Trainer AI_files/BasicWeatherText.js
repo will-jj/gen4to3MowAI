146b138bb15cdcf7e0fbca2f6e670914812650f5
William Jackson <will-jj@users.noreply.github.com>
2025-10-03 22:37:21 +0100

    Weather: Add move names to basic

--- Pokémon Gen IV Trainer AI_files/BasicWeatherText.js
+++ Pokémon Gen IV Trainer AI_files/BasicWeatherText.js
@@ -1,447 +1,537 @@
 basicWeatherAICheckText = {
 
-    ["Ampharos1"]:
-        `If the enemy has the ability Stench, then -8`,
+["Ampharos1"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Arcanine2"]:
-        `since no pokemon has ability none, no score change ever`,
+["Arcanine2"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Bellossom1"]:
-        `since no pokemon has ability none, no score change ever`,
+["Bellossom1"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Bellossom2"]:
-        `since no pokemon has ability none, no score change ever`,
+["Bellossom2"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Blastoise1"]:
-        `If the enemy has the ability Stench, then -8`,
+["Blastoise1"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Blaziken1"]:
-        `since no pokemon has ability none, no score change ever`,
+["Blaziken1"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Cacturne2"]:
-        `If the enemy has the ability Drizzle, score -8`,
+["Cacturne2"]:
+`Sandstorm:
+	If the enemy has the ability Drizzle, score -8`,
 
 
 
-    ["Camerupt1"]:
-        `If the enemy has the ability Drizzle, score -8`,
+["Camerupt1"]:
+`Sandstorm:
+	If the enemy has the ability Drizzle, score -8`,
 
 
 
-    ["Charizard1"]:
-        `since no pokemon has ability none, no score change ever`,
+["Charizard1"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Claydol3"]:
-        `since no pokemon has ability none, no score change ever`,
+["Claydol3"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Cradily3"]:
-        `since no pokemon has ability none, no score change ever`,
+["Cradily3"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Delibird1"]:
-        `If the enemy has the ability speed boost, score -8`,
+["Delibird1"]:
+`Hail:
+	If the enemy has the ability speed boost, score -8`,
 
 
 
-    ["Dragonite9"]:
-        `If the enemy has the ability Stench, then -8`,
+["Dragonite9"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Dragonite10"]:
-        `since no pokemon has ability none, no score change ever`,
+["Dragonite10"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Electabuzz2"]:
-        `If the enemy has the ability Stench, then -8`,
+["Electabuzz2"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Electrode2"]:
-        `If the enemy has the ability Stench, then -8`,
+["Electrode2"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Entei2"]:
-        `since no pokemon has ability none, no score change ever`,
+["Entei2"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Exeggutor1"]:
-        `since no pokemon has ability none, no score change ever`,
+["Exeggutor1"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Exploud2"]:
-        `since no pokemon has ability none, no score change ever`,
+["Exploud2"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Feraligatr1"]:
-        `If the enemy has the ability Stench, then -8`,
+["Feraligatr1"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Flareon4"]:
-        `since no pokemon has ability none, no score change ever`,
+["Flareon4"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Flygon2"]:
-        `since no pokemon has ability none, no score change ever`,
+["Flygon2"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Glalie1"]:
-        `If the enemy has the ability speed boost, score -8`,
+["Glalie1"]:
+`Hail:
+	If the enemy has the ability speed boost, score -8`,
 
 
 
-    ["Gloom2"]:
-        `since no pokemon has ability none, no score change ever`,
+["Gloom2"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Gorebyss2"]:
-        `If the enemy has the ability Stench, then -8`,
+["Gorebyss2"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Gyarados3"]:
-        `If the enemy has the ability Stench, then -8`,
+["Gyarados3"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Houndoom4"]:
-        `since no pokemon has ability none, no score change ever`,
+["Houndoom4"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Huntail2"]:
-        `If the enemy has the ability Stench, then -8`,
+["Huntail2"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Lanturn3"]:
-        `If the enemy has the ability Stench, then -8`,
+["Lanturn3"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Lapras6"]:
-        `If the enemy has the ability Stench, then -8`,
+["Lapras6"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Lotad1"]:
-        `If the enemy has the ability Stench, then -8 (Rain dance only)`,
+["Lotad1"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8 (Rain dance only)`,
 
 
 
-    ["Ludicolo1"]:
-        `If the enemy has the ability Stench, then -8`,
+["Ludicolo1"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Ludicolo3"]:
-        `If the enemy has the ability Stench, then -8`,
+["Ludicolo3"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Ludicolo4"]:
-        `If the enemy has the ability Stench, then -8`,
+["Ludicolo4"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Lunatone1"]:
-        `If the enemy has the ability Drizzle, score -8`,
+["Lunatone1"]:
+`Sandstorm:
+	If the enemy has the ability Drizzle, score -8`,
 
 
 
-    ["Luvdisc1"]:
-        `If the enemy has the ability Stench, then -8`,
+["Luvdisc1"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Magcargo1"]:
-        `If the enemy has the ability Drizzle, score -8`,
+["Magcargo1"]:
+`Sandstorm:
+	If the enemy has the ability Drizzle, score -8`,
 
 
 
-    ["Magneton2"]:
-        `If the enemy has the ability Stench, then -8`,
+["Magneton2"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Manectric2"]:
-        `If the enemy has the ability Stench, then -8`,
+["Manectric2"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Mantine1"]:
-        `If the enemy has the ability Stench, then -8`,
+["Mantine1"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Mantine2"]:
-        `If the enemy has the ability Stench, then -8`,
+["Mantine2"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Marill1"]:
-        `If the enemy has the ability Stench, then -8`,
+["Marill1"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Meganium1"]:
-        `since no pokemon has ability none, no score change ever`,
+["Meganium1"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Moltres3"]:
-        `since no pokemon has ability none, no score change ever`,
+["Moltres3"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Moltres5"]:
-        `since no pokemon has ability none, no score change ever`,
+["Moltres5"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Ninetales2"]:
-        `since no pokemon has ability none, no score change ever`,
+["Ninetales2"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Nosepass1"]:
-        `If the enemy has the ability Drizzle, score -8`,
+["Nosepass1"]:
+`Sandstorm:
+	If the enemy has the ability Drizzle, score -8`,
 
 
 
-    ["Numel1"]:
-        `If the enemy has the ability Drizzle, score -8`,
+["Numel1"]:
+`Sandstorm:
+	If the enemy has the ability Drizzle, score -8`,
 
 
 
-    ["Omastar2"]:
-        `If the enemy has the ability Stench, then -8`,
+["Omastar2"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Onix1"]:
-        `If the enemy has the ability Drizzle, score -8`,
+["Onix1"]:
+`Sandstorm:
+	If the enemy has the ability Drizzle, score -8`,
 
 
 
-    ["Phanpy1"]:
-        `If the enemy has the ability Drizzle, score -8`,
+["Phanpy1"]:
+`Sandstorm:
+	If the enemy has the ability Drizzle, score -8`,
 
 
 
-    ["Piloswine1"]:
-        `If the enemy has the ability speed boost, score -8`,
+["Piloswine1"]:
+`Hail:
+	If the enemy has the ability speed boost, score -8`,
 
 
 
-    ["Poliwag1"]:
-        `If the enemy has the ability Stench, then -8`,
+["Poliwag1"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Poliwhirl1"]:
-        `If the enemy has the ability Stench, then -8`,
+["Poliwhirl1"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Porygon22"]:
-        `since no pokemon has ability none, no score change ever`,
+["Porygon22"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Pupitar1"]:
-        `If the enemy has the ability Drizzle, score -8`,
+["Pupitar1"]:
+`Sandstorm:
+	If the enemy has the ability Drizzle, score -8`,
 
 
 
-    ["Raichu3"]:
-        `If the enemy has the ability Stench, then -8`,
+["Raichu3"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Raikou2"]:
-        `If the enemy has the ability Stench, then -8`,
+["Raikou2"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Rapidash3"]:
-        `since no pokemon has ability none, no score change ever`,
+["Rapidash3"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Regice2"]:
-        `If the enemy has the ability Stench, then -8`,
+["Regice2"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Regice4"]:
-        `If the enemy has the ability speed boost, score -8`,
+["Regice4"]:
+`Hail:
+	If the enemy has the ability speed boost, score -8`,
 
 
 
-    ["Sandshrew1"]:
-        `If the enemy has the ability Drizzle, score -8`,
+["Sandshrew1"]:
+`Sandstorm:
+	If the enemy has the ability Drizzle, score -8`,
 
 
 
-    ["Sandslash1"]:
-        `If the enemy has the ability Drizzle, score -8`,
+["Sandslash1"]:
+`Sandstorm:
+	If the enemy has the ability Drizzle, score -8`,
 
 
 
-    ["Sandslash2"]:
-        `If the enemy has the ability Drizzle, score -8`,
+["Sandslash2"]:
+`Sandstorm:
+	If the enemy has the ability Drizzle, score -8`,
 
 
 
-    ["Seadra1"]:
-        `If the enemy has the ability Stench, then -8`,
+["Seadra1"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Sealeo1"]:
-        `If the enemy has the ability speed boost, score -8`,
+["Sealeo1"]:
+`Hail:
+	If the enemy has the ability speed boost, score -8`,
 
 
 
-    ["Sealeo2"]:
-        `If the enemy has the ability speed boost, score -8`,
+["Sealeo2"]:
+`Hail:
+	If the enemy has the ability speed boost, score -8`,
 
 
 
-    ["Shiftry3"]:
-        `since no pokemon has ability none, no score change ever`,
+["Shiftry3"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Shuckle2"]:
-        `If the enemy has the ability Drizzle, score -8`,
+["Shuckle2"]:
+`Sandstorm:
+	If the enemy has the ability Drizzle, score -8`,
 
 
 
-    ["ShuckleLS"]:
-        `If the enemy has the ability Drizzle, score -8`,
+["ShuckleLS"]:
+`Sandstorm:
+	If the enemy has the ability Drizzle, score -8`,
 
 
 
-    ["Slowbro1"]:
-        `If the enemy has the ability Stench, then -8`,
+["Slowbro1"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Solrock1"]:
-        `If the enemy has the ability Drizzle, score -8`,
+["Solrock1"]:
+`Sandstorm:
+	If the enemy has the ability Drizzle, score -8`,
 
 
 
-    ["Spheal1"]:
-        `If the enemy has the ability speed boost, score -8`,
+["Spheal1"]:
+`Hail:
+	If the enemy has the ability speed boost, score -8`,
 
 
 
-    ["Starmie2"]:
-        `If the enemy has the ability Stench, then -8`,
+["Starmie2"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Steelix2"]:
-        `If the enemy has the ability Drizzle, score -8`,
+["Steelix2"]:
+`Sandstorm:
+	If the enemy has the ability Drizzle, score -8`,
 
 
 
-    ["Sudowoodo1"]:
-        `If the enemy has the ability Drizzle, score -8`,
+["Sudowoodo1"]:
+`Sandstorm:
+	If the enemy has the ability Drizzle, score -8`,
 
 
 
-    ["Suicune3"]:
-        `If the enemy has the ability Stench, then -8`,
+["Suicune3"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Sunkern1"]:
-        `since no pokemon has ability none, no score change ever`,
+["Sunkern1"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Surskit1"]:
-        `If the enemy has the ability Stench, then -8`,
+["Surskit1"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Trapinch1"]:
-        `If the enemy has the ability Drizzle, score -8`,
+["Trapinch1"]:
+`Sandstorm:
+	If the enemy has the ability Drizzle, score -8`,
 
 
 
-    ["Tropius2"]:
-        `since no pokemon has ability none, no score change ever`,
+["Tropius2"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Typhlosion1"]:
-        `since no pokemon has ability none, no score change ever`,
+["Typhlosion1"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Venusaur1"]:
-        `since no pokemon has ability none, no score change ever`,
+["Venusaur1"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Venusaur4"]:
-        `since no pokemon has ability none, no score change ever`,
+["Venusaur4"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Vileplume4"]:
-        `since no pokemon has ability none, no score change ever`,
+["Vileplume4"]:
+`Sunny Day:
+	Since no pokemon has ability none, no score change ever`,
 
 
 
-    ["Walrein1"]:
-        `If the enemy has the ability speed boost, score -8`,
+["Walrein1"]:
+`Hail:
+	If the enemy has the ability speed boost, score -8`,
 
 
 
-    ["Wooper1"]:
-        `If the enemy has the ability Stench, then -8`,
+["Wooper1"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Zapdos2"]:
-        `If the enemy has the ability Stench, then -8`,
+["Zapdos2"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
 
 
 
-    ["Zapdos6"]:
-        `If the enemy has the ability Stench, then -8`,
+["Zapdos6"]:
+`Rain Dance:
+	If the enemy has the ability Stench, then -8`,
+
 
 
 
