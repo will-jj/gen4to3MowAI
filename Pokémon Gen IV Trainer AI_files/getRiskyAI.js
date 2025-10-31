b23252f2305d478e988b6a098a37b6d0250904cf
William Jackson <will-jj@users.noreply.github.com>
2025-10-31 21:01:00 +0000

    Risky: remaining moves

--- Pokémon Gen IV Trainer AI_files/getRiskyAI.js
+++ Pokémon Gen IV Trainer AI_files/getRiskyAI.js
@@ -4,6 +4,24 @@ function getRiskyAI(moveData) {
 
 		switch (moveData.battleEffect) {
 			case "InflictSleep":
+			case "Selfdestruction":
+			case "MirrorMove":
+			case "Ohko":
+			case "HighCritical":
+			case "InflictConfusion":
+			case "Metronome":
+			case "Psywave":
+			case "Counter":
+			case "DestinyBond":
+			case "Swagger":
+			case "Attract":
+			case "Present":
+			case "AllStatsUpHit":
+			case "BellyDrum":
+			case "MirrorCoat":
+			case "FocusPunch":
+			case "DamageRevenge":
+			case "TeeterDance":
 				resultAiChecks.push("Risky");
 				break;
 		}
