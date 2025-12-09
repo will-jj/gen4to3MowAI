58a9a408e64c0af0c9a26ac47ed533f57ec56959
William Jackson <will-jj@users.noreply.github.com>
2025-12-09 15:57:20 +0000

    Fix: Counter,tabs

--- Pokémon Gen IV Trainer AI_files/ExpertCheckText.js
+++ Pokémon Gen IV Trainer AI_files/ExpertCheckText.js
@@ -100,7 +100,7 @@ If the last move used by foe is damaging:
 	If the last move used by the foe is special:
 		Score -1 and terminate
 	Else:
-        60.9% (156/256) chance of score +1 and terminate
+		60.9% (156/256) chance of score +1 and terminate
 
 If either of the foe's types is Physical:
 	No scoring change and terminate
