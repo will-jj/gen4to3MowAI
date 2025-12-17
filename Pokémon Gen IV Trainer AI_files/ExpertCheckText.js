5d7be688e290b697bd46619a9e4fc469a75b0466
William Jackson <will-jj@users.noreply.github.com>
2025-12-17 22:06:05 +0000

    Fix: self destruct

--- Pokémon Gen IV Trainer AI_files/ExpertCheckText.js
+++ Pokémon Gen IV Trainer AI_files/ExpertCheckText.js
@@ -845,12 +845,12 @@ If the target's evasion is boosted to +3 or more:
 
 If the user's HP is under 80%, or the user will attack after the target:
 	If the user's HP is over 50%:
-		50% (128/256) chance of score -1 and terminate
+		19.5% (50/256) chance of score -1 and terminate
 	
 	If the user's HP is under 51%:
 		50% (128/256) chance of score +1 and continue
 	
-	If the user's HP is under 30%:
+	If the user's HP is under 31%:
 		80.5% (206/256) chance of score +1 and terminate
 Else:
 	80.5% (206/256) chance of score -3 and terminate`,
