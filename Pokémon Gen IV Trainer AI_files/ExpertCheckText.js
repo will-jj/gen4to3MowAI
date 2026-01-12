89d3e025c7c9d12f777594af1acbb532c79d3d53
William Jackson <will-jj@users.noreply.github.com>
2026-01-12 21:49:14 +0000

    Fix: def up

--- Pokémon Gen IV Trainer AI_files/ExpertCheckText.js
+++ Pokémon Gen IV Trainer AI_files/ExpertCheckText.js
@@ -1163,7 +1163,7 @@ If the last move used by the foe is a special move:
 	Score -2 and terminate
 
 Otherwise:
-	5.5% (225/4096) chance of score -2 and terminate`,
+	58.6% (2401/4096) chance of score -2 and terminate`,
 
 
 ["UserEvasionUp1"]:
