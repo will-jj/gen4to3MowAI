1f91373a7c61c11f9e15bddc9858d22c76c2c8fc
William Jackson <will-jj@users.noreply.github.com>
2026-01-12 21:44:47 +0000

    Fix: spdef up

--- Pokémon Gen IV Trainer AI_files/ExpertCheckText.js
+++ Pokémon Gen IV Trainer AI_files/ExpertCheckText.js
@@ -1233,7 +1233,7 @@ If the last move used by the foe is a physical move:
 	Score -2 and terminate
 
 Otherwise:
-	5.5% (225/4096) chance of score -2 and terminate`,
+	58.6% (2401/4096) chance of score -2 and terminate`,
 
 
 ["UserSpeedUp1"]:
