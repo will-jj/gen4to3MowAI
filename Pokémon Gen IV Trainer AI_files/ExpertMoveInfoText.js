b7eaa767946a5f9ba6c04a146c3da8dd5eb2e6d0
William Jackson <will-jj@users.noreply.github.com>
2026-01-16 23:53:58 +0000

    Update ExpertMoveInfoText.js

--- Pokémon Gen IV Trainer AI_files/ExpertMoveInfoText.js
+++ Pokémon Gen IV Trainer AI_files/ExpertMoveInfoText.js
@@ -2,5 +2,5 @@ const expertMoveInfoCheckText = {
     ["Protect"]:
         `Note: The shared counter for protect/detect/endure is only updated when the move is selected by the user.`,
     ["WeatherUsing"]:
-        `Note: Moves using the current weather may have unexpected scoring when there is no weather present<br> <a class="simple" target="_blank" rel="noopener noreferrer" href="../get-weather.html">Get Weather Bug</a>`,
+        `Note: Moves using the current weather may have unexpected scoring when there is no weather present<br> <a class="simple" target="_blank" rel="noopener noreferrer" href="./get-weather.html">Get Weather Bug</a>`,
 }
