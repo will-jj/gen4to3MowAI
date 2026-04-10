10eb3c0b6e14c4cfc4e7914984a39e0174585651
William Jackson <will-jj@users.noreply.github.com>
2026-04-10 22:33:24 +0100

    Trick: short note Could be expanded

--- Pokémon Gen IV Trainer AI_files/ExpertMoveInfoText.js
+++ Pokémon Gen IV Trainer AI_files/ExpertMoveInfoText.js
@@ -3,4 +3,6 @@ const expertMoveInfoCheckText = {
         `Note: The shared counter for protect/detect/endure is only updated when the move is selected by the user.`,
     ["WeatherUsing"]:
         `Note: Moves using the current weather may have unexpected scoring when there is no weather present<br> <a class="simple" target="_blank" rel="noopener noreferrer" href="./get-weather.html">Get Weather Bug</a>`,
+    ["SwitchItems"]:
+        `Note: The AI uses the battle history struct to check the target's hold item effect.`
 }
